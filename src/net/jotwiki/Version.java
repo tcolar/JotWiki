5bb5056b220cbc459e4c336960ae7d089d31147f
tcolar <devnull@localhost>
2009-02-06 02:04:16 +0000

    fixed to parser

--- src/net/jotwiki/Version.java
+++ src/net/jotwiki/Version.java
@@ -21,7 +21,7 @@ import net.jot.utils.JOTUtilities;
  */
 public class Version 
 {
-	public static final String STAMP="02/05/2009 16:13";
+	public static final String STAMP="02/05/2009 17:59";
 	
 	public static final String VERSION="0.1.4";
 
