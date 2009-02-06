9f44c70f27d166a0cac8938f89bc43addb5ab39b
tcolar <devnull@localhost>
2009-02-06 22:34:36 +0000

    Final parser fixes

--- src/net/jotwiki/Version.java
+++ src/net/jotwiki/Version.java
@@ -21,7 +21,7 @@ import net.jot.utils.JOTUtilities;
  */
 public class Version 
 {
-	public static final String STAMP="02/06/2009 14:23";
+	public static final String STAMP="02/06/2009 14:35";
 	
 	public static final String VERSION="0.1.4";
 
